39ecc4cfc08ba9f2e8061e640fa3659529cf2941
sorien <sorien@debian.sorien>
2015-06-24 15:06:31 -0500

    lets try Travis

---
+++ travis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+./fetchIdea.sh
+
+# Run the tests
+if [ "$1" = "-d" ]; then
+    ant -d -f build-test.xml -DIDEA_HOME=./idea
+else
+    ant -f build-test.xml -DIDEA_HOME=./idea
+fi
+
+# Was our build successful?
+stat=$?
+
+if [ "${TRAVIS}" != true ]; then
+    ant -f build-test.xml -q clean
+fi
+
+# Return the build status
+exit ${stat}
